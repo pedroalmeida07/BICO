4b315cddbfe72331ab2a671e1a90f6892f76b77f
20241tiimi0011 <luigiridolfdelpiero@gmail.com>
2026-08-12 10:24:11 -0300

    Front-end do cadastro do professor concluído.

--- app/src/main/java/com/example/bico/CadastroPrestadorEmail.kt
+++ app/src/main/java/com/example/bico/CadastroPrestadorEmail.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CadastroPrestador_nome : AppCompatActivity() {
+class CadastroPrestadorEmail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro_prestador_nome)
+        setContentView(R.layout.activity_cadastro_prestador_email)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
