6c2e34704545fd0a6adc52ddc6590801a65573aa
20241tiimi0011 <luigiridolfdelpiero@gmail.com>
2026-08-13 10:24:50 -0300

    Navegabilidade (Voltar e Avançar) no cadastro do prestador

--- app/src/main/java/com/example/bico/CadastroPrestadorEmail.kt
+++ app/src/main/java/com/example/bico/CadastroPrestadorEmail.kt
@@ -1,20 +1,29 @@
 package com.example.bico
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bico.databinding.ActivityCadastroPrestadorNomeBinding
 
 class CadastroPrestadorEmail : AppCompatActivity() {
+    private lateinit var binding: ActivityCadastroPrestadorNomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro_prestador_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityCadastroPrestadorNomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Evento de clique
+
+        binding.sair.setOnClickListener {
+            // Criar o Intent para abrir a outra Activity
+            val intent = Intent(this, CadastroPrestadorServico::class.java)
+            startActivity(intent) // Inicia a nova tela
         }
     }
 }
