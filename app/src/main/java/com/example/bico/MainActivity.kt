c679f2fe96cdb18f8e1a08e9db7bed584b5130ad
Guilherme <guilhermecosta030409@gmail.com>
2026-08-13 10:13:32 -0300

    navegabilidade entre telas de inicio, login, cadastro e prestadr

--- app/src/main/java/com/example/bico/MainActivity.kt
+++ app/src/main/java/com/example/bico/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.bico
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Referenciando os elementos da UI
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+        val btnCadastrar = findViewById<Button>(R.id.btnCadastrar)
+        val txtCadastrarPrestador = findViewById<TextView>(R.id.txtCadastrarPrestador)
+
+        // Configurando a navegabilidade
+        btnEntrar.setOnClickListener {
+            val intent = Intent(this, PaginaLogin::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnCadastrar.setOnClickListener {
+            // Supondo que CadastroClienteEmail seja a primeira tela de cadastro de cliente
+            val intent = Intent(this, CadastroClienteEmail::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        txtCadastrarPrestador.setOnClickListener {
+            val intent = Intent(this, CadastroPrestadorNome::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
