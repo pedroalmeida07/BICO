c55312f207b080f6d5f5e9a204d02f69ae2be943
yanishiyama <yan.ishiyama.santos@gmail.com>
2026-08-20 09:30:28 -0300

    Refiz a tela de splash depois pode trocar as cores se quiser, mas ainda vou editar a animaçao

--- app/src/main/java/com/example/bico/MainActivity.kt
+++ app/src/main/java/com/example/bico/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.bico
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.bico.Cadastro.Cliente.CadastroClienteNome
 import com.example.bico.Cadastro.Prestador.CadastroPrestadorNome
 import com.example.bico.Login.PaginaLogin
@@ -14,7 +19,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        // Animação de saída: desce um pouco e sobe
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            val splashScreenView = splashScreenViewProvider.view
+            val slideDownUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f, 200f, -splashScreenView.height.toFloat()
+            )
+            slideDownUp.interpolator = AnticipateInterpolator(1.5f)
+            slideDownUp.duration = 800L
+
+            slideDownUp.doOnEnd { splashScreenViewProvider.remove() }
+            slideDownUp.start()
+        }
+
         //deixa a barra de status com icones pretos
         enableEdgeToEdge(statusBarStyle = SystemBarStyle.light(
             android.graphics.Color.TRANSPARENT,
