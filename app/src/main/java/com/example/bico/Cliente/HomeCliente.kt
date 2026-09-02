09d81fd4dd63596a9b3fedc43091e56bd185b26e
20241tiimi0011 <luigiridolfdelpiero@gmail.com>
2026-09-02 10:09:37 -0300

    Xml do Editar Perfil do Cliente atualizado

--- app/src/main/java/com/example/bico/Cliente/HomeCliente.kt
+++ app/src/main/java/com/example/bico/Cliente/HomeCliente.kt
@@ -13,9 +13,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bico.R
 import com.example.bico.UserRepository
+import com.example.bico.databinding.ActivityCadastroPrestadorEmailBinding
 import com.example.bico.databinding.ActivityHomeClienteBinding
 
 class HomeCliente : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeClienteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,26 +33,27 @@ class HomeCliente : AppCompatActivity() {
                 Color.WHITE
             )
         )
-        setContentView(R.layout.activity_home_cliente)
+        binding = ActivityHomeClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Mostra o primeiro nome do cliente na pagina home
         val repository = UserRepository(this)
         val usuario = repository.getUsuarioLogado()
-        val txtNomeUsuario = findViewById<TextView>(R.id.txtNomeUsuario)
+        val txtNomeUsuario = binding.txtNomeUsuario
         txtNomeUsuario.text = usuario?.primeiroNome ?: "Usuário"
 
-        findViewById<ImageView>(R.id.ic_pesquisa).setOnClickListener {
+        binding.icPesquisa.setOnClickListener {
             val intent = Intent(this, PesquisaCliente::class.java)
             startActivity(intent)
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.parentMain)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.parentMain) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        findViewById<ImageView>(R.id.imgUser).setOnClickListener {
+        binding.imgUser.setOnClickListener {
             val intent = Intent(this, EditarCliente::class.java)
             startActivity(intent)
         }
