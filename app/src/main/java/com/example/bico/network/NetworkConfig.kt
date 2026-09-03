b9491b8a9a9211a4b1b1134d63ad1496bb716542
¨Guilherme¨ <guilhermecosta030409@gmail.com>
2026-09-03 19:37:00 -0300

    VAMO LINK FUNCIONANDO

--- app/src/main/java/com/example/bico/network/NetworkConfig.kt
+++ app/src/main/java/com/example/bico/network/NetworkConfig.kt
@@ -2,5 +2,5 @@ package com.example.bico.network
 
 object NetworkConfig {
     // Se estiver usando o emulador, use 10.0.2.2 para acessar o localhost do PC
-    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "https://archlinux.tailed4748.ts.net/"
 }
