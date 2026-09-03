886aa4bcd075bb1a25b7af906c1389b9e51d7980
ldelp14 <luigiridolfdelpiero@gmail.com>
2026-09-03 15:37:40 -0300

    XML de Pesquisa, Favoritos e Denúncias completos

--- app/src/main/java/com/example/bico/Cliente/FavoritosCliente.kt
+++ app/src/main/java/com/example/bico/Cliente/FavoritosCliente.kt
@@ -1,16 +1,17 @@
-package com.example.bico
+package com.example.bico.Cliente
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bico.R
 
-class activity_configuracao_cliente : AppCompatActivity() {
+class FavoritosCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_configuracao_cliente)
+        setContentView(R.layout.activity_favoritos_cliente)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
