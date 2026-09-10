ce958e4ca275218aab1bf29abaf1f3bfd8627fb7
20241TIIMI0160 <danilodalvi1@gmail.com>
2026-09-10 08:48:50 -0300

    Adicionei a mascara de CEP e telefone

--- app/src/main/java/com/example/bico/Cadastro/CadCliente/CadastroClienteLocal.kt
+++ app/src/main/java/com/example/bico/Cadastro/CadCliente/CadastroClienteLocal.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bico.UserRepository
 import com.example.bico.databinding.ActivityCadastroClienteLocalBinding
+import com.example.bico.utils.MaskWatcher
 
 class CadastroClienteLocal : AppCompatActivity() {
     private lateinit var binding: ActivityCadastroClienteLocalBinding
@@ -29,20 +30,26 @@ class CadastroClienteLocal : AppCompatActivity() {
         binding = ActivityCadastroClienteLocalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Aplicação das máscaras
+        binding.editTextCEP.addTextChangedListener(MaskWatcher("#####-###", binding.editTextCEP))
+        binding.editTextTelefone.addTextChangedListener(MaskWatcher("(##) #####-####", binding.editTextTelefone))
+
         binding.buttonAvancar.setOnClickListener {
             val cep = binding.editTextCEP.text.toString()
+            val telefone = binding.editTextTelefone.text.toString()
             val numero = binding.editTextNumero.text.toString()
             val complemento = binding.editTextComplemento.text.toString()
 
-            if (cep.isEmpty() || numero.isEmpty()) {
-                Toast.makeText(this, "Por favor, preencha o CEP e o número", Toast.LENGTH_SHORT).show()
+            if (cep.isEmpty() || telefone.isEmpty() || numero.isEmpty()) {
+                Toast.makeText(this, "Por favor, preencha o CEP, Telefone e o número", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             // Salva no objeto temporário
             UserRepository.tempUser = UserRepository.tempUser.copy(
                 local = "$cep, $numero - $complemento",
-                cep = cep,
+                cep = MaskWatcher.unmask(cep),
+                telefone = MaskWatcher.unmask(telefone),
                 numero = numero,
                 complemento = complemento
             )
