249743f605779b9ff2d0e8c869edd24ffc67bebd
¨Guilherme¨ <guilhermecosta030409@gmail.com>
2026-09-12 18:24:59 -0300

    mudança nos métodos de requisição para a API

--- app/src/main/java/com/example/bico/network/BicoApiService.kt
+++ app/src/main/java/com/example/bico/network/BicoApiService.kt
@@ -5,15 +5,18 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface BicoApiService {
-    @POST("CadastrarCliente")
+    @POST("Cliente")
     suspend fun cadastrarCliente(@Body user: User): Response<Unit>
 
-    @POST("CadastrarPrestador")
+    @POST("Prestador")
     suspend fun cadastrarPrestador(@Body user: User): Response<Unit>
 
-    @GET("GetDadosUsuario")
-    suspend fun getDadosUsuario(@Query("id") id: String): Response<User>
+    @GET("Usuario")
+    suspend fun DadosUsuario(@Query("id") id: String): Response<User>
 
-    @POST("AtualizarUsuario")
+    @PUT ("Usuario")
     suspend fun atualizarUsuario(@Query("id") id: String, @Body user: User): Response<Unit>
+
+    @DELETE ("Usuario")
+    suspend fun deletarUsuario(@Query("id") id: String): Response<Unit>
 }
