31a702e9bb9ba518689d7577896f8b13eae04833
pedroalmeida07 <souzapedroalmeida8@gmail.com>
2026-09-22 17:23:17 -0300

    Criação do kotlin de privacidade do prestador e correção da relação do context de algumas páginas

---
+++ app/src/main/java/com/example/bico/Prestador/PrivacidadePrestador.kt
@@ -0,0 +1,32 @@
+package com.example.bico.Prestador
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bico.R
+
+class PrivacidadePrestador : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.WHITE,
+                Color.WHITE
+            )
+        )
+        setContentView(R.layout.activity_privacidade_prestador)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
